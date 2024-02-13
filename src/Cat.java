f949001309c08ca6e64bd0e276f14d387f3579f2
sigggysigler <jacksigler93@gmail.com>
2024-02-13 15:57:01 -0700

    Changed the eye height

--- src/Cat.java
+++ src/Cat.java
@@ -14,7 +14,8 @@ public class Cat {
 	private static final int EYE_HEIGHT = 20;
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
+	// change #1 - Changed the height
+	private static final int MOUTH_HEIGHT = 15;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
